40a6e50ace6c0d33a72fe750d7086fba455e6783
Diogo Bastos <diogo.bastos@neovation.local>
2017-02-21 16:57:54 -0600

    Add navigation

--- webpack.config.js
+++ webpack.config.js
@@ -22,9 +22,12 @@ module.exports = {
   resolve: {
     root: __dirname,
     alias: {
-      Main: 'app/components/Main.jsx',      
+      Main: 'app/components/Main.jsx',
       applicationStyles: 'app/styles/app.scss',
-      Navigation: 'app/components/Navigation.jsx'
+      Navigation: 'app/components/Navigation.jsx',
+      Navigation: 'app/components/Navigation.jsx',
+      Timer: 'app/components/Timer.jsx',
+      Countdown: 'app/components/Countdown.jsx',
     },
     extensions: ['', '.js', '.jsx']
   },
