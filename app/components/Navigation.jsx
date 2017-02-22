dde6b175127b4d71c6747cb39fa50c0aef2f983b
Diogo Bastos <diogo.bastos@neovation.local>
2017-02-22 09:39:21 -0600

    Create clock component

--- app/components/Navigation.jsx
+++ app/components/Navigation.jsx
@@ -7,7 +7,7 @@ var Nav = React.createClass ({
       <div className="top-bar">
         <div className="top-bar-left">
           <ul className="menu">
-            <li className="menu-text">React Time App</li>
+            <li className="menu-text">React Timer App</li>
             <li>
               <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
             </li>
